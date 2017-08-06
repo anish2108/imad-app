cf53251ada551248c011410ee6a4a5092aaaeae0
anish2108 <30771814+anish2108@users.noreply.github.com>
2017-08-06 18:13:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/ article1', function (req, res) {
+  res.send('article 1');
+});
+app.get('/ article2', function (req, res) {
+  res.send('article 2');
+});
+app.get('/ article3', function (req, res) {
+  res.send('article 3');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
